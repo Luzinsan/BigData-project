2f1325ff0e3e5a8c605de80bf3c63d10c3b747f4
team3 <team3@innopolis.university>
2025-04-12 23:03:37 +0300

    Merge branch 'main' of github.com:Luzinsan/BigData-project into main

--- src/app/scripts/import_via_sqoop.sh
+++ src/app/scripts/import_via_sqoop.sh
@@ -1,16 +1,20 @@
 username=$(sed -n 2p .env | cut -d= -f2 | tr -d "'\"")
 password=$(sed -n 3p .env | cut -d= -f2 | tr -d "'\"")
 
-# get all tables from database
-sqoop import-all-tables \
+# import all tables from database
+sqoop import-all-tables \ 
 --connect jdbc:postgresql://hadoop-04.uni.innopolis.ru/team3_projectdb \
 --username $username \
 --password $password \
 --compression-codec=snappy \
 --compress --as-parquetfile \
---warehouse-dir=/warehouse --m 1
+--warehouse-dir=project/warehouse --m 1
 
-
+# get all tables from database  
+# sqoop list-tables \
+# --connect jdbc:postgresql://hadoop-04.uni.innopolis.ru/team3_projectdb \
+# --username $username \
+# --password $password
 
 # import specific tables
 # sqoop import \
